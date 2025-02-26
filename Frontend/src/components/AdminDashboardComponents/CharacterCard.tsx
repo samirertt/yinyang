577a1b76d2c9b6298fecf37cc471caab9e256b0c
SinksWasTaken <luayhamed2005@gmail.com>
2025-02-27 01:43:20 +0300

    Made the grids responsive for both containers (Users and Characters) Made the CharacterCards container a carousel with pages that fit 8 characters at max. Added a To Do part for the AdminEditUsers page

--- Frontend/src/components/AdminDashboardComponents/CharacterCard.tsx
+++ Frontend/src/components/AdminDashboardComponents/CharacterCard.tsx
@@ -9,14 +9,16 @@ function CharacterCard(props: { img: string; name: string; Id: number; details: 
     }
     
     return(
-        <div className="grid auto-cols-max grid-cols-3 gap-4 bg-[#2F2F2F] text-[#acacaf] p-6 rounded-xl shadow-md cursor-pointer hover:bg-[#3A3A3A] transition flex flex-col items-center">
-            <div className="flex flex-col items-center">
-                <img src={character.charImage} className="mb-4 rounded-full" style={{backgroundColor:'red'}} />
-                <h3 className="text-xl font-bold text-[clamp(80%,16px,100%)]">{character.charName}</h3>
-                <p className="text-sm text-gray-400">ID: {character.charId}</p>
-                <p className="text-sm text-gray-400">{character.charUsage}%</p>
+        <div className="flex flex-col gap-4 bg-[#2F2F2F] text-[#acacaf] p-6 rounded-xl shadow-md cursor-pointer hover:bg-[#3A3A3A] transition items-center">
+            <div className="flex items-center gap-5">
+                <img src={character.charImage} className="max-w-[100px] mb-4 rounded-full" style={{backgroundColor:'red'}} />
+                <div className="items-center">
+                    <h3 className="font-bold">{character.charName}</h3>
+                    <p className="text-gray-400">ID: {character.charId}</p>
+                    <p className="text-gray-400">{character.charUsage}%</p>
+                </div>
             </div>
-            <p style={{alignSelf:"center",justifySelf:"center"}} className="col-span-2 font-bold text-[clamp(60%,36px,75%)]">{character.charDetails}</p>
+            <p style={{alignSelf:"center",justifySelf:"center"}} className="font-bold">{character.charDetails}</p>
           </div>
     )
 
--- Frontend/src/components/AdminEditUsersComponents/UserCard.tsx
+++ Frontend/src/components/AdminEditUsersComponents/UserCard.tsx
@@ -1,27 +1,31 @@
 import { useState } from 'react';
-function UserCard(props: { img: string; name: string; Id: number; role:boolean })
+function UserCard(props: {onUpdate: (arg0: number, arg1: boolean) => void, user:{ img: string; name: string; Id: number; role:boolean }})
 {
     const character = {
-        charImage:props.img,
-        charName:props.name,
-        charId:props.Id,
+        img:props.user.img,
+        name:props.user.name,
+        Id:props.user.Id,
+        role:props.user.role
     }
-    const [charRole,setCharRole] = useState(props.role);
+    const [charRole,setCharRole] = useState(props.user.role);
 
 
     const handleRole = () =>
     {
         charRole ? setCharRole(false) : setCharRole(true);
         
+        
+        
+        props.onUpdate(character.Id, charRole);
     }   
 
     
     return(
         <div className="gap-4 bg-[#2F2F2F] text-[#acacaf] p-6 rounded-xl shadow-md  hover:bg-[#3A3A3A] transition flex flex-col items-center">
             <div className="flex flex-col items-center">
-                <img src={character.charImage} className="mb-4 rounded-full" style={{backgroundColor:'red'}} />
-                <h3 className="text-xl font-bold text-[clamp(80%,16px,100%)]">{character.charName}</h3>
-                <p className="text-sm text-gray-400">ID: {character.charId}</p>
+                <img src={character.img} className="mb-4 rounded-full " style={{backgroundColor:'red'}} />
+                <h3 className="text-xl font-bold text-[clamp(80%,16px,100%)]">{character.name}</h3>
+                <p className="text-sm text-gray-400">ID: {character.Id}</p>
                 <button className='mt-2 cursor-pointer rounded-xl p-3' style={{backgroundColor:'#4a4a4a'}} onClick={handleRole}>{charRole ? <img className='opacity-50 h-[30px] object-cover' src='https://www.svgrepo.com/show/154007/manager.svg'></img> : <img className='opacity-50 h-[30px] object-cover' src='https://www.freeiconspng.com/uploads/computer-user-icon-28.png'></img>}</button>
             </div>
             
